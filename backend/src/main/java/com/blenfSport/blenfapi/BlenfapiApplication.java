0d7bcbd9729c5d097e1efc6075aa9a65ee06faf2
Brian <diazbrian47@gmail.com>
2023-10-08 19:18:08 -0500

    agregando dependecias al back

---
+++ backend/src/main/java/com/blenfSport/blenfapi/BlenfapiApplication.java
@@ -0,0 +1,13 @@
+package com.blenfSport.blenfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlenfapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlenfapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blenfSport/blenfapi/BlenfapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blenfSport.blenfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlenfapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
