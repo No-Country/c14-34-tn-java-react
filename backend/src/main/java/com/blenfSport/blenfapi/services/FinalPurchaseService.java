36e9a9f6d3b58e30c8d1cc79c20848be6be76f7d
Brian <diazbrian47@gmail.com>
2023-10-11 20:42:47 -0500

    agregadas clases Services vacias

---
+++ backend/src/main/java/com/blenfSport/blenfapi/services/FinalPurchaseService.java
@@ -0,0 +1,8 @@
+package com.blenfSport.blenfapi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FinalPurchaseService {
+
+}
---
+++ backend/src/main/java/com/blenfSport/blenfapi/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.blenfSport.blenfapi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	
+	
+	
+	
+}
