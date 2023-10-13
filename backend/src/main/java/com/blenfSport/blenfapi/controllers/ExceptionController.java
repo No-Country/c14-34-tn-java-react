11c92522884ce3a023b25e04c05722cc98466d52
FrancoBarreiro <francco.barreiiro@gmail.com>
2023-10-13 19:25:10 -0300

    Se agregan clases de excepciones

--- backend/src/main/java/com/blenfSport/blenfapi/controllers/ExceptionController.java
+++
@@ -1,5 +0,0 @@
-package com.blenfSport.blenfapi.controllers;
-//No se que poner aqui, nunca he trabajado con un controlador de excepciones
-public class ExceptionController {
-
-}
---
+++ backend/src/main/java/com/blenfSport/blenfapi/controllers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.blenfSport.blenfapi.controllers;
+
+import com.blenfSport.blenfapi.exceptions.BadRequestException;
+import com.blenfSport.blenfapi.exceptions.ErrorResponse;
+import com.blenfSport.blenfapi.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handlerBadRequest(BadRequestException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorResponse("Bad Request", ex.getMessage(), LocalDateTime.now()),
+                HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handlerResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorResponse("Resource Not Found", ex.getMessage(), LocalDateTime.now()),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<Object> handlerResourceNotFoundException(java.lang.NullPointerException ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorResponse("Resource Not Found", ex.getMessage(), LocalDateTime.now()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
