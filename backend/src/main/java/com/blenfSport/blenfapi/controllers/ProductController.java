122495158afb4a9c05aaadaf21c01a99f20ca73e
Brian <diazbrian47@gmail.com>
2023-10-17 15:46:14 -0500

    Se arreglo en metodo UpdateProduct

--- backend/src/main/java/com/blenfSport/blenfapi/controllers/ProductController.java
+++ backend/src/main/java/com/blenfSport/blenfapi/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.blenfSport.blenfapi.controllers;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,7 @@ import com.blenfSport.blenfapi.persitence.entities.Product;
 import com.blenfSport.blenfapi.services.CategoryService;
 import com.blenfSport.blenfapi.services.ProductService;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -23,47 +25,51 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
-    private final CategoryService categoryService;
+	private final ProductService productService;
+	private final CategoryService categoryService;
 
-    @PostMapping(value = "save")
-    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody @Valid ProductDto productDto,
-                                                          UriComponentsBuilder builder) {
-        Category category = categoryService.findCategoryById(productDto.category_id());
-        Product product = productService.saveProduct(productDto, category);
-        ProductResponseDto responseDto = new ProductResponseDto(product);
-        URI url = builder.path("/producto/{id}").buildAndExpand(product.getId()).toUri();
-        return ResponseEntity.created(url).body(responseDto);
+	@PostMapping(value = "save")
+	public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody @Valid ProductDto productDto,
+			UriComponentsBuilder builder) {
+		Category category = categoryService.findCategoryById(productDto.category_id());
+		Product product = productService.saveProduct(productDto, category);
+		ProductResponseDto responseDto = new ProductResponseDto(product);
+		URI url = builder.path("/producto/{id}").buildAndExpand(product.getId()).toUri();
+		return ResponseEntity.created(url).body(responseDto);
 
-    }
+	}
 
-    @GetMapping(value = "show")
-    public ResponseEntity<Page<ProductResponseDto>> showAllProducts(Pageable pageable) {
-        return ResponseEntity.ok(productService.getAllProducts(pageable).map(ProductResponseDto::new));
+	@GetMapping(value = "show")
+	public ResponseEntity<Page<ProductResponseDto>> showAllProducts(Pageable pageable) {
+		return ResponseEntity.ok(productService.getAllProducts(pageable).map(ProductResponseDto::new));
 
-    }
+	}
 
-    @GetMapping("/{name}")
-    public ResponseEntity<Page<ProductResponseDto>> showAllProductsByCategory(Pageable pageable,
-                                                                              @PathVariable String name) {
+	@GetMapping("/{name}")
+	public ResponseEntity<Page<ProductResponseDto>> showAllProductsByCategory(Pageable pageable,
+			@PathVariable String name) {
 
-        Category categoryId = categoryService.FindCategoryIdByname(name);
-        return ResponseEntity.ok(productService.FindProductsByCategory(pageable, categoryId.getId()).map(ProductResponseDto::new));
+		Category categoryId = categoryService.FindCategoryIdByname(name);
+		return ResponseEntity
+				.ok(productService.FindProductsByCategory(pageable, categoryId.getId()).map(ProductResponseDto::new));
 
+	}
 
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build();
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
-    }
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<ProductResponseDto> updateProduct(@RequestBody @Valid ProductDto productDto, @PathVariable Long id) {
+		Category category = categoryService.findCategoryById(productDto.category_id());
+		Optional<Product> productOptional = productService.FindProductById(id);
+		Product product = productOptional.get();
 
-    @PutMapping
-    public ResponseEntity<?> updateProduct(@RequestBody @Valid ProductDto productDto) {
-        Category category = new Category();
-        category.setId(productDto.category_id());
-        Product product = new Product(productDto, category);
-        return ResponseEntity.ok(new ProductResponseDto(productService.updateProduct(product)));
-    }
+		product.UpdateProduct(productDto, category);
+
+		return ResponseEntity.ok(new ProductResponseDto(product));
+	}
 }
