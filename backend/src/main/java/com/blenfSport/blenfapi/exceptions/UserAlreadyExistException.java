560f28e38ecba9e889d3f386cabf0e61f0023e52
Brian <diazbrian47@gmail.com>
2023-10-29 16:08:15 -0500

    añadido metodo para mostrar datos del usuario logeado

---
+++ backend/src/main/java/com/blenfSport/blenfapi/exceptions/UserAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.blenfSport.blenfapi.exceptions;
+
+public class UserAlreadyExistException extends RuntimeException {
+	
+	public UserAlreadyExistException(String message) {
+		super(message);
+	}
+
+}
--- backend/src/main/java/com/blenfSport/blenfapi/services/JwtService.java
+++ backend/src/main/java/com/blenfSport/blenfapi/services/JwtService.java
@@ -13,15 +13,20 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
+	
 	private static final String SECRET_KEY = "586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
-
+	
 	public String getToken(UserDetails user) {
 		return getToken(new HashMap<>(), user);
 	}
+	
+
 
 	private String getToken(HashMap<String, Object> extraClaims, UserDetails user) {
 
