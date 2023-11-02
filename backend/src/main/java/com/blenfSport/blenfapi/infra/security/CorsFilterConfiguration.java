1ae449d22f811358356a292875e3575d386d9d63
Brian <diazbrian47@gmail.com>
2023-11-02 00:21:58 -0500

    cors configuration

--- backend/src/main/java/com/blenfSport/blenfapi/infra/security/CorsFilterConfiguration.java
+++ backend/src/main/java/com/blenfSport/blenfapi/infra/security/CorsFilterConfiguration.java
@@ -5,22 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class CorsFilterConfiguration {
-	
-	@Bean
-	  public WebMvcConfigurer corsConfigurer() {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowCredentials(false)
+                    .maxAge(3600);
             }
         };
     }
 }
+
 
 
