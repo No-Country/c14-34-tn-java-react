d71aab8f28d018c95a89908df934d673ee427537
Luciano Ponce <poncestigaardluciano@gmail.com>
2023-11-02 16:09:30 -0300

    se corrije el link de compras

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -89,7 +89,7 @@ function Navbar() {
                         <NavLink to={"/Perfil"}>Mi Perfil</NavLink>
                       </li>
                       <li className="UserInfoMenu">
-                        <NavLink to={"/ListaCompras"}>Mis compras</NavLink>
+                        <NavLink to={"/Compras"}>Mis compras</NavLink>
                       </li>
                       <li className="UserInfoMenu">
                         <button className="cerrarSesion" onClick={handleLogout}>
