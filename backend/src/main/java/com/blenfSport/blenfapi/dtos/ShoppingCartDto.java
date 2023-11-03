19293f54d6b1b20a9682a0c2adb9913b6b5f9173
Brian <diazbrian47@gmail.com>
2023-11-03 10:39:15 -0500

    ya se puede agregar productos al carrito

--- backend/src/main/java/com/blenfSport/blenfapi/dtos/ShoppingCartDto.java
+++ backend/src/main/java/com/blenfSport/blenfapi/dtos/ShoppingCartDto.java
@@ -1,15 +1,11 @@
 package com.blenfSport.blenfapi.dtos;
 
-import com.blenfSport.blenfapi.persitence.entities.Product;
-import com.blenfSport.blenfapi.persitence.entities.User;
 
 import jakarta.validation.constraints.NotNull;
 
 public record ShoppingCartDto(
 		@NotNull
-		Product product,
-		@NotNull
-		User user,
+		Long id,
 		@NotNull
 		Integer amount
 		) {
