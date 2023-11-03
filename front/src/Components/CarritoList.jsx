7cd523abfda8e306508d2d0687ee68531e146f69
Brian <diazbrian47@gmail.com>
2023-11-03 12:52:56 -0500

    integrada funcion para eliminar productos del cart

--- front/src/Components/CarritoList.jsx
+++ front/src/Components/CarritoList.jsx
@@ -6,7 +6,17 @@ import React, { useEffect } from "react";
 
 function CarritoList(){
     const [items, setItems] = useState([]);
-    const CarritoItemList = async()=> {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalMessage, setModalMessage] = useState("");
+    const openModal = (message) => {
+        setModalMessage(message);
+        setIsModalOpen(true);
+        setTimeout(() => {
+        setIsModalOpen(false);
+        }, 1000);
+
+      };
+      const CarritoItemList = async()=> {
         try{
             const token = localStorage.getItem('token');
             const response = await axios.get(`http://18.220.229.238/shoppingCart`, {
@@ -25,19 +35,52 @@ function CarritoList(){
             console.error('Error al obtener detalles de la compra', error);
         }
     }
+    const deleteItem = async(itemId)=> {
+        console.log(itemId)
+        try{
+            const token = localStorage.getItem('token');
+            const response = await axios.delete(`http://18.220.229.238/shoppingCart/detele/${itemId}`, {
+                headers: {
+                  'Authorization': `Bearer ${token}`
+                }
+              })
+              if(response.status === 200){
+                openModal("Eliminado con exito")
+                CarritoItemList()
+              }else{
+                throw new Error('No se pudo eliminar');
+              }
+
+
+        }catch(error){
+            console.error('Error al eliminar', error);
+        }
+    }
+
+
+
     useEffect(() => {
         
         CarritoItemList();
       }, []);
 
 return(
-   <div className="carritoList-Container" >
+    <div className="carritoList-Container" >
+    {isModalOpen && (
+   <div className="modal-cart">
+     <div className="modal-cart-content">
+       <p>{modalMessage}</p>
+     </div>
+   </div>
+ )}
+    
+   
    <h1 className="carritoList-title">Mi Carrito</h1>
    {items.length === 0 ? (
     <p>No hay productos en el carrito</p>
    ): (items.map((item) => (
                 <div key={item.id} className="carritoList-card">
-                    <button className="carritoList-btn">X</button>
+                    <button className="carritoList-btn" onClick={() => {deleteItem(item.id);}}>X</button>
                     <p>Producto: {item.product.name}</p>
                     <p>Precio: ${item.product.price}</p>
                     <p>Color: {item.product.color}</p>
@@ -45,39 +88,9 @@ return(
                     <p>Cantidad: {item.amount}</p>
                 </div>
             )))}
-            <button className="carritoList-btn">Comprar</button>
+            {items.length > 0 && <button className="carritoList-btn">Comprar</button>}
    </div>
 );
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default CarritoList
